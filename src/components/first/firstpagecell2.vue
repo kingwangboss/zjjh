c1c6c80e3e34119b1e37cc94a7799212654585fd
OneJ <3942241@qq.com>
2018-05-22 14:43:30 +0800

    +

--- src/components/first/firstpagecell2.vue
+++ src/components/first/firstpagecell2.vue
@@ -4,19 +4,19 @@
 
           <div class="left">
               <div class="top">
-                  <div class="m_content" style="border-right:1px solid rgb(234, 234, 234)">{{cell.Name}}</div>
+                  <div class="m_content_l">{{cell.Name}}</div>
                   <div style="border-right:1px solid rgb(234, 234, 234)" class="m_content">{{cell.PlanSection}}</div>
-                  <div class="m_content" style="color:blue" v-if="cell.DsType === 0">
+                  <div class="m_content_r" style="color:rgb(102,170,247);" v-if="cell.DsType === 0">
                       <span class="btn">{{cell.EndIndex ? cell.EndIndex+' ' : ""}}定</span>
                   </div>
-                  <div class="m_content" style="color:red" v-else-if="cell.DsType === 1">
+                  <div class="m_content_r" style="color:rgb(255,76,90);" v-else-if="cell.DsType === 1">
                       <span class="btn1">{{cell.EndIndex ? cell.EndIndex+' ' : ""}}杀</span>
                   </div>
                   <div v-else></div>
               </div>
               
-              <div class="bottom" style="color:blue;" v-if="cell.DsType === 0">{{cell.GuessValue}}</div>
-              <div class="bottom" style="color:red;" v-else-if="cell.DsType === 1">{{cell.GuessValue}}</div>
+              <div class="bottom" style="color:rgb(102,170,247);" v-if="cell.DsType === 0">{{cell.GuessValue}}</div>
+              <div class="bottom" style="color:rgb(255,76,90);" v-else-if="cell.DsType === 1">{{cell.GuessValue}}</div>
           </div>
 
           <div class="right">
@@ -32,19 +32,19 @@
 
           <div class="left">
               <div class="top">
-                  <div class="m_content" style="border-right:1px solid rgb(234, 234, 234)">{{cell.Name}}</div>
+                  <div class="m_content_l">{{cell.Name}}</div>
                   <div style="border-right:1px solid rgb(234, 234, 234)" class="m_content">{{cell.PlanSection}}</div>
-                  <div class="m_content" style="color:blue" v-if="cell.DsType === 0">
+                  <div class="m_content_r" style="color:rgb(102,170,247);" v-if="cell.DsType === 0">
                       <span class="btn">{{cell.EndIndex ? cell.EndIndex+' ' : ""}}定</span>
                   </div>
-                  <div class="m_content" style="color:red" v-else-if="cell.DsType === 1">
+                  <div class="m_content_r" style="color:rgb(255,76,90);" v-else-if="cell.DsType === 1">
                       <span class="btn1">{{cell.EndIndex ? cell.EndIndex+' ' : ""}}杀</span>
                   </div>
                   <div v-else></div>
               </div>
               
-              <div class="bottom" style="color:blue;" v-if="cell.DsType === 0">{{cell.GuessValue}}</div>
-              <div class="bottom" style="color:red;" v-else-if="cell.DsType === 1">{{cell.GuessValue}}</div>
+              <div class="bottom" style="color:rgb(102,170,247);" v-if="cell.DsType === 0">{{cell.GuessValue}}</div>
+              <div class="bottom" style="color:rgb(255,76,90);" v-else-if="cell.DsType === 1">{{cell.GuessValue}}</div>
           </div>
 
           <div class="right">
@@ -106,25 +106,37 @@
     line-height: 8vw;
   }
 
+  .m_content_l{
+      border-right:1px solid rgb(234, 234, 234); 
+      color:black;
+      width:38vw;
+      padding-left:10px;
+    //   text-align:center;
+  }
+  .m_content_r{
+      padding:0 10px;
+      font-size: 3vw;
+  }
+
   .m_content {
       padding:0 10px;
       font-size: 3vw;
-      color: gray;
+      color: rgb(170,170,170);
       .btn {
         padding: 2px 6px;
         min-width: 50px;
-        border: 1px solid blue;
+        border: 1px solid rgb(102,170,247);;
         border-radius: 3vw;
         font-size: 3vw;
-        color: blue;
+        color:rgb(102,170,247);
       }
       .btn1 {
         padding: 2px 6px;
         min-width: 50px;
-        border: 1px solid red;
+        border: 1px solid rgb(255,76,90);
         border-radius: 3vw;
         font-size: 3vw;
-        color: red;
+        color:rgb(255,76,90);
       }
     }
   .bottom {
@@ -188,22 +200,22 @@ export default {
         this.$draw.drawMain(
           time_canvas,
           value,
-          "rgb(76,179,85)",
-          "rgb(195,195,195)"
+          "rgb(53,198,76)",
+          "rgb(198,198,198)"
         );
       } else if (60 <= parseInt(value) && 80 > parseInt(value)) {
         this.$draw.drawMain(
           time_canvas,
           value,
-          "rgb(86,165,96)",
-          "rgb(195,195,195)"
+          "rgb(251,158,23)",
+          "rgb(198,198,198)"
         );
       } else {
         this.$draw.drawMain(
           time_canvas,
           value,
-          "rgb(200,0,30)",
-          "rgb(195,195,195)"
+          "rgb(255,76,90)",
+          "rgb(198,198,198)"
         );
       }
     }
